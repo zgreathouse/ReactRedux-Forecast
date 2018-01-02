3c081373ef5e03a0485ac98c278529473bc05532
zgreathouse <greathouse.zachary@gmail.com>
2018-01-01 22:43:55 -0800

    build out action creator for fetching the weather, convert SearchBar into a container component and hook up the fetchWeather action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,20 @@
-import { API_KEY } from '../../config/keys';
+import axios from 'axios';
+
+// import { API_KEY } from '../../config/keys';
+const API_KEY = '3255cdd54b782323dc4392d231977cc8';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+//type constant
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+//sync action creator
+export const fetchWeather = (city) => {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+  console.log(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+};
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,33 +1,37 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions';
 
 class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { term: ''};
+    this.state = { term: '' };
 
     this.onInputChange = this.onInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   onInputChange(event) {
-    this.setState({term: event.target.input})
+    this.setState({ term: event.target.value })
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    //Fetch weather data
+
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' });
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit} className="input-group">
         <input
-          placeholder="Get a five-day forecast in your favorite cities"
+          placeholder="Get a five-day forecast of your favorite cities"
           className="form-control"
-          value={this.state.term}
           onChange={this.onInputChange}
+          value={this.state.term}
         />
         <span className="input-group-btn">
           <button
@@ -42,9 +46,8 @@ class SearchBar extends Component {
   }
 }
 
-// const mapStateToProps = (state) => ({
-//
-// })
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
 
-// export default connect(mapStateToProps)(SearchBar);
-export default SearchBar;
+export default connect(null, mapDispatchToProps)(SearchBar);
