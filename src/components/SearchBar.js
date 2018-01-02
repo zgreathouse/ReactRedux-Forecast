9fb7ceac0e0a03cee4e2e8c837821bd97aa1a9f3
zgreathouse <greathouse.zachary@gmail.com>
2018-01-01 21:48:50 -0800

    create handleSubmit helper method for the onSubmit propety of form

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,16 +6,23 @@ class SearchBar extends Component {
     super(props);
 
     this.state = { term: ''};
+
     this.onInputChange = this.onInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   onInputChange(event) {
     this.setState({term: event.target.input})
   }
 
+  handleSubmit(event) {
+    event.preventDefault();
+    //Fetch weather data
+  }
+
   render() {
     return (
-      <form className="input-group">
+      <form onSubmit={this.handleSubmit} className="input-group">
         <input
           placeholder="Get a five-day forecast in your favorite cities"
           className="form-control"
@@ -23,7 +30,10 @@ class SearchBar extends Component {
           onChange={this.onInputChange}
         />
         <span className="input-group-btn">
-          <button type="submit" className="btn btn-secondary">
+          <button
+            type="submit"
+            className="btn btn-secondary"
+          >
             Submit
           </button>
         </span>
