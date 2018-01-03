dfe0087173ff83aa0147131cc9c0bc06bffdab22
zgreathouse <greathouse.zachary@gmail.com>
2018-01-02 19:17:02 -0800

    refactor weatheReducer

--- src/reducers/weatherReducer.js
+++ src/reducers/weatherReducer.js
@@ -3,7 +3,7 @@ import { FETCH_WEATHER } from '../actions';
 const weatherReducer = (state = [], action) => {
   switch(action.type) {
     case FETCH_WEATHER:
-      return [ action.payload.data ];
+      return [action.payload.data, ...state]
     default:
       return state;
   };
